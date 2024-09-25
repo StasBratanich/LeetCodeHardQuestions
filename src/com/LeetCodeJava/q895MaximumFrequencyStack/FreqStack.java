6d87892342d1757309d40fd447b3968138f53b59
סטס ברטניץ <bstasb@gmail.com>
2024-09-25 17:49:41 +0300

    895. Maximum Frequency Stack

--- src/com/LeetCodeJava/q895MaximumFrequencyStack/FreqStack.java
+++ src/com/LeetCodeJava/q895MaximumFrequencyStack/FreqStack.java
@@ -1,19 +1,40 @@
 package com.LeetCodeJava.q895MaximumFrequencyStack;
 
-import java.util.Collections;
 import java.util.*;
 
 public class FreqStack {
-    Stack<Integer> stack;
+    int maxFreq;
+    HashMap<Integer, Integer> freqMap;
+    HashMap<Integer, Stack<Integer>> freqStack;
+
     public FreqStack() {
-        stack = new Stack<>();
+        maxFreq = 0;
+        freqMap = new HashMap<>();
+        freqStack = new HashMap<>();
     }
 
     public void push(int val) {
+        int freq = freqMap.getOrDefault(val,0) + 1;
+        freqMap.put(val, freq);
+
+        if (freq > maxFreq) {
+            maxFreq = freq;
+        }
+
+        freqStack.computeIfAbsent(freq, f->new Stack()).push(val);
 
     }
 
     public int pop() {
-        return 0;
+        Stack<Integer> temp = freqStack.get(maxFreq);
+        int topVal = temp.pop();
+
+        if (temp.isEmpty()) {
+            maxFreq--;
+        }
+
+        freqMap.put(topVal, freqMap.get(topVal) - 1);
+
+        return topVal;
     }
 }
